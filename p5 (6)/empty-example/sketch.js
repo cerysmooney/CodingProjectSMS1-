feba03966945a061417a8aa92c365a3f02c8472b
cerys <cerys2.mooney@live.uwe.ac.uk>
2021-12-16 10:35:44 +0000

    Update sketch.js

--- p5 (6)/empty-example/sketch.js
+++ p5 (6)/empty-example/sketch.js
@@ -1,8 +1,24 @@
 function setup() {
-  createCanvas(400,600)
-  // put setup code here
+  createCanvas(600,600)
+  background(200)
 }
 
 function draw() {
-  background(200)
+  background(102);
+  push();
+  translate(width * 0.8, height * 0.5);
+  rotate(frameCount / 60.0);
+  polygon(0, 0, 70, 7);
+  pop();
 }
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
